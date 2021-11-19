dbb898ae9b2df4ca500fb2787a52a6f694ae2503
abyss_USB <85128487+abyssquest@users.noreply.github.com>
2021-11-19 18:20:22 +0900

    1119 class end

--- R_Rstudio/script/chap06_DataHandling.R
+++ R_Rstudio/script/chap06_DataHandling.R
@@ -181,7 +181,7 @@ long <- melt(wide_read, id='id')
 long
 
 # 컬럼명 수정
-colnames(long) <- c("id", "Data", "Buy")
+colnames(long) <- c("id", "Date", "Buy")
 head(long)
 
 # reshape2 패키지의 smiths 데이터 셋 구조 변경하기
@@ -219,17 +219,3 @@ class(acast) # "array"
 
 # 월 단위 variable(대기관련 컬럼) 컬럼 합계
 acast(air_melt, month~variable, sum)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
